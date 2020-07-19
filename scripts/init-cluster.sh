3f2cdfa963fa3cbf0aa5dbc412ed318d9c8f13e6
Brian Lachniet <blachniet@gmail.com>
2020-07-19 16:51:11 -0400

    Do not remove taint from master

--- scripts/init-cluster.sh
+++ scripts/init-cluster.sh
@@ -5,13 +5,8 @@ set -e
 # Initialize the master node.
 kubeadm init
 
-export KUBECONFIG=/etc/kubernetes/admin.conf
-
 # Install Calico (pod network add-on).
-kubectl apply -f https://docs.projectcalico.org/v3.14/manifests/calico.yaml
-
-# Allow master to schedule pods.
-kubectl taint nodes --all node-role.kubernetes.io/master-
+KUBECONFIG=/etc/kubernetes/admin.conf kubectl apply -f https://docs.projectcalico.org/v3.14/manifests/calico.yaml
 
 # Create a file indicating that we ran this script.
 touch /usr/local/src/kubeadm-initialized
